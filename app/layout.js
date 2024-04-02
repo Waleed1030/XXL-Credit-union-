4364440b6ef322e04d948864bed7433cbf7bd6b2
Eh1z <godsentehiz11@gmail.com>
2024-04-02 18:11:52 +0100

    added test chatbot

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { EdgeStoreProvider } from './lib/edgestore'
 import { Cabin, M_PLUS_1 } from 'next/font/google'
 import './ui/globals.css'
+import Script from 'next/script'
 
 
 const cabin = Cabin({ subsets: ['latin'] });
@@ -13,10 +14,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <>
     <html lang="en">
+    <Script src="//code.tidio.co/kuktzqesnhuiucdqtraxzhlwgtbkotbd.js" async>
       <body className={cabin.className}>
         <EdgeStoreProvider>{children}</EdgeStoreProvider>
       </body>
+    </Script>
     </html>
+    </>
   )
 }
